90c810bae08e1e1586de977c58ade15dc262c76f
Programmer12m <malishahid975@gmail.com>
2025-09-02 00:15:44 +0500

    may the final touch or  nay need touches

--- src/BookMangment/BookFind.java
+++ src/BookMangment/BookFind.java
@@ -1,20 +1,19 @@
 package BookMangment;
 
 public class BookFind extends Book {
-    String serialNumb;
 
-    public void find(Book book,String  serialNumb) {
-        if (book == null) {
+    public void find(Book o,String  serialNumb) {
+        if (o == null) {
             System.out.println("Book object is null");
             return;
         }
 
-        if (!serialNumb.equals(book.serialNUmb)) {
+        if (!serialNumb.equals(o.serialNUmb)) {
             System.out.println(" Can't find");
         } else {
-            System.out.println(book.getAuthor());
-            System.out.println(book.getBookTitle());
-            System.out.println(book.getSerialNUmb());
+            System.out.println(o.getAuthor());
+            System.out.println(o.getBookTitle());
+            System.out.println(o.getSerialNUmb());
         }
     }
 }
